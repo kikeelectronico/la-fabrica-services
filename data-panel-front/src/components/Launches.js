ef82fa00e6ba2b91d9c9932415824c8d170f273e
enrique.gomez <enrique.gomez@bq.com>
2022-07-07 22:21:48 +0200

    delete dinamic alerts at launches

--- data-panel-front/src/components/Launches.js
+++ data-panel-front/src/components/Launches.js
@@ -6,7 +6,6 @@ import React, { useState, useEffect } from "react";
   export default function Launches() {
   
     const [data, setData] = useState({launches_flag: false, fail_to_update: true});
-    const [uv_alert, setUvAlert] = useState("0, 0, 0");
     const [api_requested, setApiRequested] = useState(false);
   
     useEffect(() => {
@@ -22,21 +21,6 @@ import React, { useState, useEffect } from "react";
         .then((data) => setData(data))
         .catch((error) => console.log(error))
         .finally(() => setApiRequested(true))
-    }
-  
-    useEffect(() => {
-      evaluateAlerts()
-    }, [data])
-  
-    const evaluateAlerts = () => {
-      /*if (data.weather_flag) {
-        var uv = data.weather.current.uv
-        if (uv <= 2) setUvAlert("0,0,0")
-        else if (uv <= 5) setUvAlert("255,255,0")
-        else if (uv <= 7) setUvAlert("255,126,0")
-        else if (uv <= 10) setUvAlert("255,0,0")
-        else setUvAlert("126,0,35")
-      }*/
     }
 
     const getLaunchTime = (launch) => {
@@ -66,12 +50,7 @@ import React, { useState, useEffect } from "react";
 
                 {
                   launch !== undefined ?    
-                    <div
-                      className="launchesCard"
-                      style={{
-                        boxShadow: "0 0.1rem 1rem rgba(" + uv_alert  + ", 0.8)"
-                      }}
-                    >
+                    <div className="launchesCard">
                       <div className="launchesName">
                         {getMisionName()}
                       </div>
