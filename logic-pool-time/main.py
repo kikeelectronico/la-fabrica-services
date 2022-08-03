e42f3aa47cb250475624361d7f25dc6336dd1bbb
enrique.gomez <enrique.gomez@bq.com>
2022-08-03 22:17:01 +0200

    change the hood strategy

--- logic-pool-time/main.py
+++ logic-pool-time/main.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     today = datetime.datetime.now()
     hour = today.strftime("%H:%M:%S")
 
-    if hour == "09:00:00":
+    if hour == "08:00:00":
       homeware.setParam("hood001", "on", True)
-    elif hour == "11:00:00":
-      homeware.setParam("hood001", "on", False)
     elif hour == "12:00:00":
+      homeware.setParam("hood001", "on", False)
+    elif hour == "22:00:00":
       homeware.setParam("hood001", "on", True)
-    elif hour == "21:00:00":
+    elif hour == "06:00:00":
       homeware.setParam("hood001", "on", False)
