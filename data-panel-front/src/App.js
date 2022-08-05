bfbe350b28249c1eed9bee2b2f27c1d7d08bb467
enrique.gomez <enrique.gomez@bq.com>
2022-08-06 00:39:25 +0200

    set everything up for cocina panel

--- data-panel-front/src/App.js
+++ data-panel-front/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/cocina" element={<Cocina/>} />
