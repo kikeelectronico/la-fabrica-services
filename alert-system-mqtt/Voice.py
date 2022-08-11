cf2556d14572af6c5fe40b94e42eb533775dc303
enrique.gomez <enrique.gomez@bq.com>
2022-08-11 18:28:43 +0200

    voice control

--- alert-system-mqtt/Voice.py
+++ alert-system-mqtt/Voice.py
@@ -2,6 +2,8 @@ from google.cloud import texttospeech
 import threading
 import os
 
+from gtts import gTTS
+
 DEVICES_IPS = ['192.168.10.201', '192.168.10.202', '192.168.10.203']
 
 class Voice:
@@ -17,32 +19,36 @@ class Voice:
     )
 
   def getFile(self, text):
-      file_name = text
-      for c in ((" ", ""),("ó","o"),(".", ""),("á", "a"),("é", "e"),("í", "i"),("ú", "u"),("!", ""),("¡", ""),("?", ""),("¿", "")):
-          file_name = file_name.replace(*c)
-      file_path = "audio/" + file_name+".mp3"
+        file_name = text
+        for c in ((" ", ""),("ó","o"),(".", ""),("á", "a"),("é", "e"),("í", "i"),("ú", "u"),("!", ""),("¡", ""),("?", ""),("¿", "")):
+            file_name = file_name.replace(*c)
+        file_path = "audio/" + file_name+".mp3"
 
-      if os.path.exists(file_path):
-          print("Already exists")
-      else:
-          synthesis_input = texttospeech.SynthesisInput(text=text)
+        if os.path.exists(file_path):
+            print("Already exists")
+        else:
+            # synthesis_input = texttospeech.SynthesisInput(text=text)
 
-          response = self.text_to_speech_client.synthesize_speech(
-              input=synthesis_input, voice=self.voice, audio_config=self.audio_config
-          )
+            # response = self.text_to_speech_client.synthesize_speech(
+            #     input=synthesis_input, voice=self.voice, audio_config=self.audio_config
+            # )
 
-          with open(file_path, "wb") as out:
-              out.write(response.audio_content)
-      return file_path
+            # with open(file_path, "wb") as out:
+            #     out.write(response.audio_content)
+        
+            t2s = gTTS(text=text, tld="es", lang='es', slow=False) 
+            t2s.save(file_path)
+
+        return file_path
 
   def playFile(self, file_path):
     class runCommandThread(threading.Thread):
-      def __init__(self, command):
-          threading.Thread.__init__(self)
-          self.cmd = command
+        def __init__(self, command):
+            threading.Thread.__init__(self)
+            self.cmd = command
 
-      def run(self):
-          os.system(self.cmd)
+        def run(self):
+            os.system(self.cmd)
 
     threads = []
     for ip in DEVICES_IPS:
