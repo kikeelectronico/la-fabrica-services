ee2311d667972c2ee5b69df2766b9ed7466556a6
enrique.gomez <enrique.gomez@bq.com>
2022-08-17 22:37:39 +0200

    init the mqtt client

--- alert-system-request/main.py
+++ alert-system-request/main.py
@@ -21,30 +21,29 @@ public_IP_saved = 'unknow'
 mqtt_client = mqtt.Client()
 
 def getHomewareTest():
-    response = requests.get("https://" + HOMEWARE_DOMAIN + "/test").text
-    return response
+  response = requests.get("https://" + HOMEWARE_DOMAIN + "/test").text
+  return response
 
 def getPublicIP():
-    ip = requests.get(GET_IP_ENDPOINT).text
-    return ip
+  ip = requests.get(GET_IP_ENDPOINT).text
+  return ip
 
 def main():
-    global public_IP_saved
-    while True:
-        try:
-            ip = getPublicIP()
-            if not ip == public_IP_saved:
-                #bot.send_message(ENRIQUE_CHAT_ID, 'Cambio de IP pública de Coruscant\r\n\r\n Actual: ' + str(ip) + '\r\nAbandonada: ' + str(public_IP_saved))
-                mqtt_client.publish("voice-alerts", "Cambio de I P pública")
-                public_IP_saved = ip
+  mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
+  mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
+  global public_IP_saved
+  while True:
+    ip = getPublicIP()
+    if not ip == public_IP_saved:
+      #bot.send_message(ENRIQUE_CHAT_ID, 'Cambio de IP pública de Coruscant\r\n\r\n Actual: ' + str(ip) + '\r\nAbandonada: ' + str(public_IP_saved))
+      mqtt_client.publish("voice-alerts", "Cambio de IP pública")
+      public_IP_saved = ip
 
-            if not getHomewareTest() == 'Load':
-                #bot.send_message(ENRIQUE_CHAT_ID, '*Homeware* caido', parse_mode= 'Markdown')
-                mqtt_client.publish("voice-alerts", "Homeware se ha caido")
+    if not getHomewareTest() == 'Load':
+      #bot.send_message(ENRIQUE_CHAT_ID, '*Homeware* caido', parse_mode= 'Markdown')
+      mqtt_client.publish("voice-alerts", "Homeware se ha caido")
 
-            time.sleep(SLEEP_TIME)
-        except:
-            time.sleep(10)
+    time.sleep(SLEEP_TIME)
 
 if __name__ == "__main__":
 	main()
