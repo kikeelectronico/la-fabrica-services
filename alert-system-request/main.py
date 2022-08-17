740cf85e6a6c188f3cae9e6b62d622a35aa8aca5
enrique.gomez <enrique.gomez@bq.com>
2022-08-17 21:48:06 +0200

    use the voice alert microservice

--- alert-system-request/main.py
+++ alert-system-request/main.py
@@ -1,5 +1,4 @@
-import telebot
-from Voice import Voice
+import paho.mqtt.client as mqtt
 import os
 import time
 import requests
@@ -8,15 +7,17 @@ if os.environ.get("MQTT_PASS", "pass") == "pass":
   from dotenv import load_dotenv
   load_dotenv(dotenv_path="../.env")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "no_token")
-ENRIQUE_CHAT_ID = os.environ.get("ENRIQUE_CHAT_ID", "no_id")
+MQTT_USER = os.environ.get("MQTT_USER", "user")
+MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
+MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
+MQTT_PORT = 1883
 HOMEWARE_DOMAIN = os.environ.get("HOMEWARE_DOMAIN", "localhost")
+
 SLEEP_TIME = 10
 
-bot = telebot.TeleBot(token=BOT_TOKEN)
-voice = Voice()
-
 public_IP_saved = 'unknow'
+
+mqtt_client = mqtt.Client()
 
 def getHomewareTest():
     response = requests.get("https://" + HOMEWARE_DOMAIN + "/test").text
@@ -32,13 +33,13 @@ def main():
         try:
             ip = getPublicIP()
             if not ip == public_IP_saved:
-                bot.send_message(ENRIQUE_CHAT_ID, 'Cambio de IP pública de Coruscant\r\n\r\n Actual: ' + str(ip) + '\r\nAbandonada: ' + str(public_IP_saved))
-                voice.getAndPlay("Cambio de I P pública")
+                #bot.send_message(ENRIQUE_CHAT_ID, 'Cambio de IP pública de Coruscant\r\n\r\n Actual: ' + str(ip) + '\r\nAbandonada: ' + str(public_IP_saved))
+                mqtt_client.publish("voice-alerts", "Cambio de I P pública")
                 public_IP_saved = ip
 
             if not getHomewareTest() == 'Load':
-                bot.send_message(ENRIQUE_CHAT_ID, '*Homeware* caido', parse_mode= 'Markdown')
-                voice.getAndPlay("Homeware se ha caido")
+                #bot.send_message(ENRIQUE_CHAT_ID, '*Homeware* caido', parse_mode= 'Markdown')
+                mqtt_client.publish("voice-alerts", "Homeware se ha caido")
 
             time.sleep(SLEEP_TIME)
         except:
