36b9c921c6d5807572a311b0412109d4d3fb5516
enrique.gomez <enrique.gomez@bq.com>
2022-08-17 21:00:16 +0200

    hi voice alert microservice

---
+++ voice-alert/main.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import os
+
+from Voice import Voice
+
+if os.environ.get("MQTT_PASS", "pass") == "pass":
+  from dotenv import load_dotenv
+  load_dotenv(dotenv_path="../.env")
+
+MQTT_USER = os.environ.get("MQTT_USER", "user")
+MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
+MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
+MQTT_PORT = 1883
+
+TOPICS = ["voice-alerts"]
+
+mqtt_client = mqtt.Client()
+voice = Voice()
+
+def on_message(client, userdata, msg):
+  payload = msg.payload.decode('utf-8').replace("\'", "\"")
+  voice.getAndPlay(payload)
+  print(payload)
+
+def on_connect(client, userdata, flags, rc):
+    for topic in TOPICS:
+        client.subscribe(topic)
+	
+if __name__ == "__main__":
+	mqtt_client.on_message = on_message
+	mqtt_client.on_connect = on_connect
+
+	mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
+	mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
+	mqtt_client.loop_forever()
+
+    
