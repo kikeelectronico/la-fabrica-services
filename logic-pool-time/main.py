3b0727b54c752fe25cbfb02ea56176d25e177663
enrique.gomez <enrique.gomez@bq.com>
2022-08-18 01:17:00 +0200

    initialize the mqtt client

--- logic-pool-time/main.py
+++ logic-pool-time/main.py
@@ -1,11 +1,24 @@
 import paho.mqtt.client as mqtt
 import datetime
+import os
+
 from homeware import Homeware
+
+if os.environ.get("MQTT_PASS", "pass") == "pass":
+  from dotenv import load_dotenv
+  load_dotenv(dotenv_path="../.env")
+
+MQTT_USER = os.environ.get("MQTT_USER", "user")
+MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
+MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
+MQTT_PORT = 1883
 
 mqtt_client = mqtt.Client()
 homeware = Homeware(mqtt_client)
 
 if __name__ == "__main__":
+  mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
+  mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
   while True:
     today = datetime.datetime.now()
     hour = today.strftime("%H:%M:%S")
