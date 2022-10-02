a10fad0ce1979cf46b2256e4a36f198e465a07a3
enrique.gomez <enrique.gomez@bq.com>
2022-10-02 21:17:22 +0200

    disable temporarily switch003

--- logic-pool-mqtt/switches.py
+++ logic-pool-mqtt/switches.py
@@ -1,12 +1,13 @@
 def green(homeware, topic, payload):
   if topic == "device/control":
-    if payload["id"] == "switch003" and payload["param"] == "on":
-      status = payload["value"]
-      control_ids=["light001", "light002"]
-      for control_id in control_ids:
-        homeware.execute(control_id, "on", status)
+    pass
+    # if payload["id"] == "switch003" and payload["param"] == "on":
+    #   status = payload["value"]
+    #   control_ids=["light001", "light002"]
+    #   for control_id in control_ids:
+    #     homeware.execute(control_id, "on", status)
   
-  elif topic == "device/switch003/on":
-    control_ids=["light001", "light002"]
-    for control_id in control_ids:
-      homeware.execute(control_id, "on", payload)
+  # elif topic == "device/switch003/on":
+  #   control_ids=["light001", "light002"]
+  #   for control_id in control_ids:
+  #     homeware.execute(control_id, "on", payload)
