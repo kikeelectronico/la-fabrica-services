02a7ec844bdf1567c9aecf9284c08b2c00b69a39
enrique.gomez <enrique.gomez@bq.com>
2022-10-07 21:25:36 +0200

    switch

--- logic-pool-mqtt/switches.py
+++ logic-pool-mqtt/switches.py
@@ -1,13 +1,12 @@
 def green(homeware, topic, payload):
   if topic == "device/control":
-    pass
-    # if payload["id"] == "switch003" and payload["param"] == "on":
-    #   status = payload["value"]
-    #   control_ids=["light001", "light002"]
-    #   for control_id in control_ids:
-    #     homeware.execute(control_id, "on", status)
+    if payload["id"] == "switch003" and payload["param"] == "on":
+      status = payload["value"]
+      control_ids=["light001", "light002"]
+      for control_id in control_ids:
+        homeware.execute(control_id, "on", status)
   
-  # elif topic == "device/switch003/on":
-  #   control_ids=["light001", "light002"]
-  #   for control_id in control_ids:
-  #     homeware.execute(control_id, "on", payload)
+  elif topic == "device/switch003/on":
+    control_ids=["light001", "light002"]
+    for control_id in control_ids:
+      homeware.execute(control_id, "on", payload)
