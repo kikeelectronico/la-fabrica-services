293af580c4d0cf8ac4cc2f750622e7dc39071584
enrique.gomez <enrique.gomez@bq.com>
2022-10-12 16:11:58 +0200

    add method to get status by id and param

--- logic-pool-mqtt/Homeware.py
+++ logic-pool-mqtt/Homeware.py
@@ -1,11 +1,16 @@
 import json
+import requests
 
 class Homeware:
 
   __mqtt_client = None
+  __host = "localhost"
+  __token = "token"
 
-  def __init__(self, mqtt_client):
+  def __init__(self, mqtt_client, host, token):
     self.__mqtt_client = mqtt_client
+    self.__host = host
+    self.__token = token
 
   def execute(self, id, param, value):    
     control_payload = {
@@ -15,6 +20,12 @@ class Homeware:
       "intent": "execute"
     }
     self.__mqtt_client.publish("device/control", json.dumps(control_payload))
+
+  def get(self, id, param):
+    url = self.__host + "/status/get/" + id
+    headers = {"Authorization": "baerer " + self.__token}
+    response = requests.get(url, headers=headers)
+    return response.json()[param]
   
   def voiceAlert(self, text):
     self.__mqtt_client.publish("voice-alerts", text)
--- logic-pool-mqtt/main.py
+++ logic-pool-mqtt/main.py
@@ -16,6 +16,9 @@ MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
 MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
 MQTT_PORT = 1883
 
+HOMEWARE_DOMAIN = os.environ.get("HOMEWARE_DOMAIN", "localhost")
+HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no-token")
+
 TOPICS = [
   "device/control",
   "device/switch003/on",
@@ -28,7 +31,7 @@ TOPICS = [
   ]
 
 mqtt_client = mqtt.Client(client_id="logic-pool-mqtt")
-homeware = Homeware(mqtt_client)
+homeware = Homeware(mqtt_client, HOMEWARE_DOMAIN, HOMEWARE_API_KEY)
 
 def on_message(client, userdata, msg):
   payload = functions.loadPayload(msg.payload)
