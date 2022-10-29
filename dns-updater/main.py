b206b9f3e607e4589c5bfa22fdc9288c7c2306bb
enrique.gomez <enrique.gomez@bq.com>
2022-10-29 22:19:59 +0200

    dns-updater

---
+++ dns-updater/main.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import time
+import paho.mqtt.client as mqtt
+import json
+
+if os.environ.get("GET_IP_ENDPOINT", "none") == "none":
+  from dotenv import load_dotenv
+  load_dotenv(dotenv_path="../.env")
+
+GET_IP_ENDPOINT = os.environ.get("GET_IP_ENDPOINT", "none")
+CLOUDFLARE_ZONE = os.environ.get("CLOUDFLARE_ZONE", "none")
+CLOUDFLARE_DNS_ID = os.environ.get("CLOUDFLARE_DNS_ID", "none")
+CLOUDFLARE_TOKEN = os.environ.get("CLOUDFLARE_TOKEN", "none")
+MQTT_USER = os.environ.get("MQTT_USER", "user")
+MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
+MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
+MQTT_PORT = 1883
+
+last_ip = "unknown"
+mqtt_client = mqtt.Client(client_id="dns-updater")
+
+if __name__ == "__main__":
+  while True:
+    ip = requests.get(GET_IP_ENDPOINT).text
+    if not ip == last_ip and len(ip.split(".")) == 4:
+      if not CLOUDFLARE_ZONE == "none"and not CLOUDFLARE_DNS_ID == "none" and not CLOUDFLARE_ZONE == "none":
+        url = "https://api.cloudflare.com/client/v4/zones/" + CLOUDFLARE_ZONE + "/dns_records/" + CLOUDFLARE_DNS_ID
+        payload="{\"content\": \"" + ip + "\"}"
+        headers = {
+          'Authorization': 'Bearer ' + CLOUDFLARE_TOKEN,
+          'Content-Type': 'application/json'
+        }
+        response = requests.request("PATCH", url, headers=headers, data=payload).json()
+
+        if response["success"]:
+          mqtt_client.publish("message-alerts", "IP actualizada")
+        else:
+          mqtt_client.publish("message-alerts", "Problemas al actualizar la IP")
+        last_ip = ip
+
+    time.sleep(10)
+    
