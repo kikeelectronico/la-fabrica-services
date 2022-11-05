6443007c9c944732c59195ac78c5a4dbeade254c
enrique.gomez <enrique.gomez@bq.com>
2022-11-05 23:58:28 +0100

    keep the state if equal

--- logic-pool-mqtt/power.py
+++ logic-pool-mqtt/power.py
@@ -6,7 +6,7 @@ BATHROOM_POWER = 800
 HEATER_POWER = 2200
 MAX_POWER = 3500
 
-def shouldHeat(homeware, thermostat_id):
+def shouldHeat(homeware, thermostat_id, radiator_id):
   if homeware.get(thermostat_id,"activeThermostatMode") == "heat":
     ambient = homeware.get(thermostat_id,"thermostatTemperatureAmbient")
     set_point =  homeware.get(thermostat_id,"thermostatTemperatureSetpoint")
@@ -15,7 +15,7 @@ def shouldHeat(homeware, thermostat_id):
     elif ambient > set_point:
       return False
     else:
-      return False
+      return homeware.get(radiator_id,"on")
   else:
     return False
 
@@ -24,11 +24,11 @@ def powerManagment(homeware, topic, payload):
   if not homeware.get("scene_ducha", "deactivate"):
     livingroom = False
     bedroom = False
-    bathroom = shouldHeat(homeware, "termos")
+    bathroom = shouldHeat(homeware, "termos", "radiator003")
     heater = True
   elif homeware.get("scene_ducha", "deactivate"):
-    livingroom = shouldHeat(homeware, "termos")
-    bedroom = shouldHeat(homeware, "termos")
+    livingroom = shouldHeat(homeware, "termos", "radiator001")
+    bedroom = shouldHeat(homeware, "termos", "radiator002")
     bathroom = False
     heater = not livingroom
   
