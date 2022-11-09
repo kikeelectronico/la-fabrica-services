1d7c361433d62a29edc69ef892dc1d022f7cc90e
enrique.gomez <enrique.gomez@bq.com>
2022-11-09 22:17:51 +0100

    manage power alerts directly

--- logic-pool-mqtt/power.py
+++ logic-pool-mqtt/power.py
@@ -6,6 +6,9 @@ BATHROOM_POWER = 800
 HEATER_POWER = 2200
 MAX_POWER = 3500
 
+power_timestamp = 0
+power_alert = False
+
 def shouldHeat(homeware, thermostat_id, radiator_id):
   if homeware.get("scene_power_alert","deactivate"):
     if homeware.get(thermostat_id,"activeThermostatMode") == "heat":
@@ -23,17 +26,34 @@ def shouldHeat(homeware, thermostat_id, radiator_id):
     return False
 
 def powerManagment(homeware, topic, payload): 
-  # Logic
-  if not homeware.get("scene_ducha", "deactivate"):
+  global power_timestamp
+  global power_alert
+
+  if homeware.get("current001", "brightness") >= 100:
+    power_alert = True
+    power_timestamp = time.time()
+
+  if homeware.get("current001", "brightness") < 40 and power_alert and (time.time() - power_timestamp) > 30:
+    power_alert = False
+
+  if not power_alert:
+    if not homeware.get("scene_ducha", "deactivate"):
+      livingroom = False
+      bedroom = False
+      bathroom = shouldHeat(homeware, "termos", "radiator003")
+      heater = True
+    elif homeware.get("scene_ducha", "deactivate"):
+      livingroom = shouldHeat(homeware, "termos", "radiator001")
+      bedroom = shouldHeat(homeware, "termos", "radiator002")
+      bathroom = False
+      heater = not livingroom
+  else:
     livingroom = False
     bedroom = False
-    bathroom = shouldHeat(homeware, "termos", "radiator003")
-    heater = homeware.get("scene_power_alert","deactivate")
-  elif homeware.get("scene_ducha", "deactivate"):
-    livingroom = shouldHeat(homeware, "termos", "radiator001")
-    bedroom = shouldHeat(homeware, "termos", "radiator002")
     bathroom = False
-    heater = not livingroom and homeware.get("scene_power_alert","deactivate")
+    heater = False
+  
+
   
   # Set values
   homeware.execute("radiator001","on",livingroom)
