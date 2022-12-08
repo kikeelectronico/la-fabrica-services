f587ef5e00fbdaa6e5821e7aae30a82227ecd8fd
enrique.gomez <enrique.gomez@bq.com>
2022-12-08 01:12:18 +0100

    add basic rules for heat control

--- logic-pool-time/main.py
+++ logic-pool-time/main.py
@@ -35,17 +35,39 @@ if __name__ == "__main__":
     if hour == "06:00:00" and not hour == just_executed:
       just_executed = hour
       homeware.execute("hood001", "on", False)
-    elif hour == "07:00:00" and not hour == just_executed:
+    elif hour == "07:30:00" and not hour == just_executed:
       just_executed = hour
       homeware.execute("scene_noche", "deactivate", True)
-      homeware.execute("termos", "thermostatTemperatureSetpoint", 21)
-      homeware.execute("termos", "thermostatMode", "heat")
+      # Weekday control
+      weekday = today.weekday()
+      if weekday in [0,1,2,3,4]:
+        homeware.execute("thermostat_dormitorio", "thermostatTemperatureSetpoint", 23)
+        homeware.execute("thermostat_dormitorio", "thermostatMode", "heat")
+        homeware.execute("termos", "thermostatTemperatureSetpoint", 23)
+        homeware.execute("termos", "thermostatMode", "heat")
     elif hour == "08:00:00" and not hour == just_executed:
       just_executed = hour
       homeware.execute("hood001", "on", True)
+    elif hour == "09:00:00" and not hour == just_executed:
+      just_executed = hour
+      # Weekday control
+      weekday = today.weekday()
+      if weekday in [0,1,2,3,4]:
+        homeware.execute("thermostat_dormitorio", "thermostatTemperatureSetpoint", 19)
+        homeware.execute("termos", "thermostatTemperatureSetpoint", 22)
+      elif weekday in [5,6]:
+        homeware.execute("thermostat_dormitorio", "thermostatTemperatureSetpoint", 23)
+        homeware.execute("thermostat_dormitorio", "thermostatMode", "heat")
+        homeware.execute("termos", "thermostatTemperatureSetpoint", 23)
+        homeware.execute("termos", "thermostatMode", "heat")
     elif hour == "12:00:00" and not hour == just_executed:
       just_executed = hour
       homeware.execute("hood001", "on", False)
+      # Weekday control
+      weekday = today.weekday()
+      if weekday in [5,6]:
+        homeware.execute("thermostat_dormitorio", "thermostatTemperatureSetpoint", 19)
+        homeware.execute("termos", "thermostatTemperatureSetpoint", 22)
     elif hour == "22:00:00" and not hour == just_executed:
       just_executed = hour
       homeware.execute("hood001", "on", True)
