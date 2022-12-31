213592c40f42a4adce81a8513369a7b0e6a6bc9a
enrique.gomez <enrique.gomez@bq.com>
2022-12-31 13:11:01 +0100

    add device hearbeat monitor

--- heartbeat-monitor/main.py
+++ heartbeat-monitor/main.py
@@ -11,11 +11,12 @@ MQTT_USER = os.environ.get("MQTT_USER", "user")
 MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
 MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
 MQTT_PORT = 1883
-TOPICS = ["heartbeats", "heartbeats/request"]
+TOPICS = ["heartbeats", "heartbeats/request", "device/heartbeat"]
 
 mqtt_client = mqtt.Client(client_id="heartbeat-monitor")
 
-heartbeats = {}
+microservices_heartbeats = {}
+devices_heartbeats = {}
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
@@ -27,17 +28,31 @@ def on_message(client, userdata, msg):
 	if msg.topic in TOPICS:
 		if msg.topic == "heartbeats/request":
 			current_time = time.time()
+			# Control microservices heartbeats
 			services_to_delete = []
-			for service in heartbeats.keys():
-				if current_time - heartbeats[service] > 70:
+			for service in microservices_heartbeats.keys():
+				if current_time - microservices_heartbeats[service] > 70:
 					mqtt_client.publish("message-alerts", service.decode("utf-8") + ": caido")
 					services_to_delete.append(service)
 			if len(services_to_delete) > 0:
 				for service in services_to_delete:
-					del heartbeats[service]
-		else:
+					del microservices_heartbeats[service]
+			# Control devices heartbeats
+			services_to_delete = []
+			for service in devices_heartbeats.keys():
+				if current_time - devices_heartbeats[service] > 70:
+					mqtt_client.publish("message-alerts", service.decode("utf-8") + ": caido")
+					#mqtt_client.publish("voice-alerts", service.decode("utf-8") + " no responde")
+					services_to_delete.append(service)
+			if len(services_to_delete) > 0:
+				for service in services_to_delete:
+					del devices_heartbeats[service]
+		elif msg.topic == "heartbeats":
 			service = msg.payload
-			heartbeats[service] = time.time()
+			microservices_heartbeats[service] = time.time()
+		elif msg.topic == "device/heartbeat":
+			service = msg.payload
+			devices_heartbeats[service] = time.time()
 
 
 if __name__ == "__main__":
