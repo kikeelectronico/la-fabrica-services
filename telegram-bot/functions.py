b15c594c47b070daefad5d14f80aa3c6d1a171ab
enrique.gomez <enrique.gomez@bq.com>
2023-02-05 22:35:52 +0100

    move functions

--- telegram-bot/functions.py
+++ telegram-bot/functions.py
@@ -16,3 +16,32 @@ def getPublicIP():
 def getHomewareTest():
     response = requests.get("https://" + HOMEWARE_API_HOST + "/test").text
     return response
+
+def test():
+  return "I think this is broken. It has a hole."
+
+def downloadYouTubeVideo(url):
+    if not 'list' in url:
+        video = YouTube(url)
+        video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    else:
+        playlist = Playlist(url)
+        for video in playlist.videos:
+            video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+    # Get the mp4 files
+    all_in_dir = os.listdir('.')
+    files = [something for something in all_in_dir if something.endswith('.mp4')]
+    # Get the first file
+    urls = []
+    for file in files:
+        # Upload to the bucket
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(file)
+        blob.upload_from_filename(file)
+        # Delete the local file
+        os.remove(file)
+        # Add URL
+        urls.append("https://storage.cloud.google.com/" + BUCKET_NAME + "/" + file.replace(" ", "%20"))
+    # Return URL
+    return urls
