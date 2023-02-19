06fced03d4a65d8220e819c1e72e6b6846febb7e
enrique.gomez <enrique.gomez@bq.com>
2023-02-19 01:50:50 +0100

    catch an error on encoding and pass

--- logic-pool-mqtt/functions.py
+++ logic-pool-mqtt/functions.py
@@ -4,11 +4,14 @@ def payloadToBool(value):
   return value in ["True", "true"]
 
 def loadPayload(payload):
-  payload = payload.decode('utf-8').replace("\'", "\"")
+  try:
+    payload = payload.decode('utf-8').replace("\'", "\"")
 
-  if payload in ["True", "true", "False", "false"]:
-    return payload in ["True", "true"]
-  elif "}" in payload or "]" in payload:
-    return json.loads(payload)
-  else:
-    return payload
+    if payload in ["True", "true", "False", "false"]:
+      return payload in ["True", "true"]
+    elif "}" in payload or "]" in payload:
+      return json.loads(payload)
+    else:
+      return payload
+  except:
+    return None
