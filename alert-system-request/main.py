0300a76f5eb4d5a222fb998a80178fe905528e5b
enrique.gomez <enrique.gomez@bq.com>
2023-02-26 18:32:41 +0100

    add hue bridge env vars

--- alert-system-request/main.py
+++ alert-system-request/main.py
@@ -13,6 +13,8 @@ MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
 MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
 HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "no_set")
 HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no_set")
+HUE_HOST = os.environ.get("HUE_HOST", "no_set")
+HUE_TOKEN = os.environ.get("HUE_TOKEN", "no_set")
 
 # Define constants
 MQTT_PORT = 1883
@@ -39,6 +41,12 @@ if __name__ == "__main__":
   if HOMEWARE_API_KEY == "no_set":
     print("HOMEWARE_API_KEY env vars no set")
     exit()
+  if HUE_HOST == "no_set":
+    print("HUE_HOST env vars no set")
+    exit()
+  if HUE_TOKEN == "no_set":
+    print("HUE_TOKEN env vars no set")
+    exit()
 
   # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
@@ -52,7 +60,7 @@ if __name__ == "__main__":
       mqtt_client.publish("voice-alerts", "Homeware no responde")
       mqtt_client.publish("message-alerts", "Homeware no responde")
     # Verify Hue Bridge connectivity
-    if not functions.hueTest(HOMEWARE_API_URL, HOMEWARE_API_KEY):
+    if not functions.hueTest(HUE_HOST, HUE_TOKEN):
       mqtt_client.publish("voice-alerts", "Hue bridge no responde")
       mqtt_client.publish("message-alerts", "Hue bridge no responde")
     # Send heartbeart
