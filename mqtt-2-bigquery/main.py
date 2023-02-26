3fe278605766ec3d6650ef282907f3002821737d
enrique.gomez <enrique.gomez@bq.com>
2023-02-26 13:46:33 +0100

    clean code and check env vars

--- mqtt-2-bigquery/main.py
+++ mqtt-2-bigquery/main.py
@@ -1,22 +1,22 @@
-import requests
 import time
 import paho.mqtt.client as mqtt
-import json
 import os
 from google.cloud import bigquery
 
-if os.environ.get("MQTT_PASS", "pass") == "pass":
+# Load env vars
+if os.environ.get("MQTT_PASS", "no_set") == "no_set":
   from dotenv import load_dotenv
   load_dotenv(dotenv_path="../.env")
 
-MQTT_USER = os.environ.get("MQTT_USER", "user")
-MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
-MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
-MQTT_PORT = 1883
-POWER_DDBB = os.environ.get("POWER_DDBB", "no_ddbb")
-AMBIENT_DDBB = os.environ.get("AMBIENT_DDBB", "no_ddbb")
-POWER_CONSTANT = 35
+MQTT_USER = os.environ.get("MQTT_USER", "no_set")
+MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
+MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
+POWER_DDBB = os.environ.get("POWER_DDBB", "no_set")
+AMBIENT_DDBB = os.environ.get("AMBIENT_DDBB", "no_set")
 
+# Define constants
+MQTT_PORT = 1883
+POWER_CONSTANT = 35
 TOPICS = [
 	"heartbeats/request",
 	"device/current001/brightness",
@@ -28,9 +28,7 @@ TOPICS = [
 	"device/thermostat_dormitorio/thermostatHumidityAmbient"
 ]
 
-mqtt_client = mqtt.Client(client_id="mqtt-2-bigquery")
-bigquery_client = bigquery.Client()
-
+# Declare variables
 latest_power = 0
 latest_livingroom_temperature = 0
 latest_bathroom_temperature = 0
@@ -39,6 +37,11 @@ latest_livingroom_humidity = 0
 latest_bathroom_humidity = 0
 latest_bedroom_humidity = 0
 
+# Instantiate objects
+mqtt_client = mqtt.Client(client_id="mqtt-2-bigquery")
+bigquery_client = bigquery.Client()
+
+# Change the type of the payload
 def typifyPayload(topic, payload):
 	if "heartbeats" in topic:
 		return payload
@@ -47,12 +50,12 @@ def typifyPayload(topic, payload):
 	else:
 		return int(payload)
 
-
+# Suscribe to topics on connect
 def on_connect(client, userdata, flags, rc):
-	# Suscribe to topics
 	for topic in TOPICS:
 		client.subscribe(topic)
 
+# Do tasks when a message is received
 def on_message(client, userdata, msg):
 	global latest_power
 	global latest_livingroom_temperature
@@ -70,64 +73,111 @@ def on_message(client, userdata, msg):
 	elif topic == "device/current001/brightness" and payload != latest_power:
 		sendPowerRequest(payload)
 		latest_power = payload
-	elif topic == "device/thermostat_livingroom/thermostatTemperatureAmbient" and payload != latest_livingroom_temperature:
-		sendThermostatRequest(payload, last_value=latest_livingroom_temperature, location="livingroom", magnitude="temperature", units="C")
+	elif topic == "device/thermostat_livingroom/thermostatTemperatureAmbient" \
+				and payload != latest_livingroom_temperature:
+		sendThermostatRequest(payload, 
+													last_value=latest_livingroom_temperature,
+													location="livingroom",
+													magnitude="temperature",
+													units="C")
 		latest_livingroom_temperature = payload
-	elif topic == "device/thermostat_bathroom/thermostatTemperatureAmbient" and payload != latest_bathroom_temperature:
-		sendThermostatRequest(payload, last_value=latest_bathroom_temperature, location="bathroom", magnitude="temperature", units="C")
+	elif topic == "device/thermostat_bathroom/thermostatTemperatureAmbient" \
+				and payload != latest_bathroom_temperature:
+		sendThermostatRequest(payload, 
+													last_value=latest_bathroom_temperature,
+													location="bathroom",
+													magnitude="temperature",
+													units="C")
 		latest_bathroom_temperature = payload
-	elif topic == "device/thermostat_dormitorio/thermostatTemperatureAmbient" and payload != latest_bedroom_temperature:
-		sendThermostatRequest(payload, last_value=latest_bedroom_temperature, location="bedroom", magnitude="temperature", units="C")
+	elif topic == "device/thermostat_dormitorio/thermostatTemperatureAmbient" \
+				and payload != latest_bedroom_temperature:
+		sendThermostatRequest(payload, 
+													last_value=latest_bedroom_temperature,
+													location="bedroom",
+													magnitude="temperature",
+													units="C")
 		latest_bedroom_temperature = payload
-	elif topic == "device/thermostat_livingroom/thermostatHumidityAmbient" and payload != latest_livingroom_humidity:
-		sendThermostatRequest(payload, last_value=latest_livingroom_humidity, location="livingroom", magnitude="humidity", units="%")
+	elif topic == "device/thermostat_livingroom/thermostatHumidityAmbient" \
+				and payload != latest_livingroom_humidity:
+		sendThermostatRequest(payload, 
+													last_value=latest_livingroom_humidity,
+													location="livingroom",
+													magnitude="humidity",
+													units="%")
 		latest_livingroom_humidity = payload
-	elif topic == "device/thermostat_bathroom/thermostatHumidityAmbient" and payload != latest_bathroom_humidity:
-		sendThermostatRequest(payload, last_value=latest_bathroom_humidity, location="bathroom", magnitude="humidity", units="%")
+	elif topic == "device/thermostat_bathroom/thermostatHumidityAmbient" \
+				and payload != latest_bathroom_humidity:
+		sendThermostatRequest(payload, 
+													last_value=latest_bathroom_humidity,
+													location="bathroom",
+													magnitude="humidity",
+													units="%")
 		latest_bathroom_humidity = payload
-	elif topic == "device/thermostat_dormitorio/thermostatHumidityAmbient" and payload != latest_bedroom_humidity:
-		sendThermostatRequest(payload, last_value=latest_bedroom_humidity, location="bedroom", magnitude="humidity", units="%")
+	elif topic == "device/thermostat_dormitorio/thermostatHumidityAmbient" \
+				and payload != latest_bedroom_humidity:
+		sendThermostatRequest(payload, 
+													last_value=latest_bedroom_humidity,
+													location="bedroom",
+													magnitude="humidity",
+													units="%")
 		latest_bedroom_humidity = payload
 
+# Insert data into the databse
 def sendPowerRequest(payload):
 	ts = int(time.time())
-	query_job = bigquery_client.query(
+	bigquery_client.query(
 			"""
 				INSERT INTO `{}`
 				(time, power, version)
 				VALUES ({},{},4);
 			""".format(POWER_DDBB, ts, payload * POWER_CONSTANT)
-	).result()
+	)
 
+# Insert data into the databse
 def sendThermostatRequest(payload, last_value, location, magnitude, units):
 	ts = int(time.time())
-	query_job = bigquery_client.query(
+	bigquery_client.query(
 		"""\
 			INSERT INTO `{}`\
 			(time, magnitude, value, location, units)\
 			VALUES ({},"{}",{},"{}","{}");\
 		""".format(AMBIENT_DDBB, ts, magnitude, last_value, location, units)
-	).result()
-	query_job = bigquery_client.query(
+	)
+	bigquery_client.query(
 		"""\
 			INSERT INTO `{}`\
 			(time, magnitude, value, location, units)\
 			VALUES ({},"{}",{},"{}","{}");\
 		""".format(AMBIENT_DDBB, ts, magnitude, payload, location, units)
-	).result()
+	)
+
+# Main entry point
+if __name__ == "__main__":
+	# Check env vars
+	if MQTT_HOST == "no_set":
+		print("MQTT_HOST env vars no set")
+		exit()
+	if MQTT_PASS == "no_set":
+		print("MQTT_PASS env vars no set")
+		exit()
+	if MQTT_HOST == "no_set":
+		print("MQTT_HOST env vars no set")
+		exit()
+	if POWER_DDBB == "no_set":
+		print("POWER_DDBB env vars no set")
+		exit()
+	if AMBIENT_DDBB == "no_set":
+		print("AMBIENT_DDBB env vars no set")
+		exit()
 		
-def main():
-  # Define callbacks
+	# Declare the callback functions
 	mqtt_client.on_message = on_message
 	mqtt_client.on_connect = on_connect
-  # Create connection with the MQTT broker	
+  # Connect to the mqtt broker
 	mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
 	mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
-  # Send boot message
+  # Wake up alert
 	mqtt_client.publish("message-alerts", "MQTT 2 BigQuery: operativo")
-  # Begin the loop
+  # Main loop
 	mqtt_client.loop_forever()
-
-if __name__ == "__main__":
-	main()
  
