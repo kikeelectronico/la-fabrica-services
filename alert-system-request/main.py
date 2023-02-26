c1923a6d18aed53df09ff29fd7ae58810eb36988
enrique.gomez <enrique.gomez@bq.com>
2023-02-26 12:56:23 +0100

    verify env vars before using them

--- alert-system-request/main.py
+++ alert-system-request/main.py
@@ -8,12 +8,12 @@ if os.environ.get("MQTT_PASS", "pass") == "pass":
   from dotenv import load_dotenv
   load_dotenv(dotenv_path="../.env")
 
-MQTT_USER = os.environ.get("MQTT_USER", "user")
-MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
-MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
+MQTT_USER = os.environ.get("MQTT_USER", "no_set")
+MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
+MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
 MQTT_PORT = 1883
-HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "localhost")
-HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no_key")
+HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "no_set")
+HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no_set")
 
 # Define constants
 SLEEP_TIME = 10
@@ -23,6 +23,24 @@ mqtt_client = mqtt.Client(client_id="alert-system-requests")
 
 # Main entry point
 if __name__ == "__main__":
+  # Check env vars
+  if MQTT_HOST == "no_set":
+    print("MQTT_HOST env vars no set")
+    exit()
+  if MQTT_PASS == "no_set":
+    print("MQTT_PASS env vars no set")
+    exit()
+  if MQTT_HOST == "no_set":
+    print("MQTT_HOST env vars no set")
+    exit()
+  if HOMEWARE_API_URL == "no_set":
+    print("HOMEWARE_API_URL env vars no set")
+    exit()
+  if HOMEWARE_API_KEY == "no_set":
+    print("HOMEWARE_API_KEY env vars no set")
+    exit()
+
+  # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
   mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
   # Wake up alert
