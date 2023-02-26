6a3b7ea9b3d85d8c94b6cd23ab48e76e21eb279c
enrique.gomez <enrique.gomez@bq.com>
2023-02-26 18:18:43 +0100

    the cache is no longer needed

--- logic-pool-mqtt/general.py
+++ logic-pool-mqtt/general.py
@@ -1,30 +1,10 @@
 
 HUMIDITY_TRIGGER = 40
 
-sensors_cache = {}
-
 # Control the bathroom hood
 def hood(homeware, topic, payload):
-  global sensors_cache
   if topic in ["device/thermostat_bathroom","device/switch_hood/on"]:
-    # Cache the state of the bathroom's thermostat
-    if not "thermostat_bathroom" in sensors_cache.keys():  
-      sensors_cache["thermostat_bathroom"] = {
-        "thermostatHumidityAmbient": homeware.get("thermostat_bathroom", "thermostatHumidityAmbient")
-      }
-    if topic == "device/thermostat_bathroom":
-      sensors_cache["thermostat_bathroom"] = payload
-    # Cache the state of the hood's switch
-    if not "switch_hood" in sensors_cache.keys():  
-      sensors_cache["switch_hood"] = {
-        "on": homeware.get("switch_hood", "on")
-      }
-    if topic == "device/switch_hood/on":
-      sensors_cache["switch_hood"] = {
-        "on": payload
-      }
-    # Control the hood
-    state = (sensors_cache["thermostat_bathroom"]["thermostatHumidityAmbient"] > HUMIDITY_TRIGGER) or sensors_cache["switch_hood"]["on"]
+    state = (homeware.get("thermostat_bathroom", "thermostatHumidityAmbient") > HUMIDITY_TRIGGER) or homeware.get("switch_hood", "on")
     homeware.execute("hood001", "on", state)
 
 # Toggle the kitchen light
