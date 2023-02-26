6996820133a51901c1c0f5a6a2cf202e259da687
enrique.gomez <enrique.gomez@bq.com>
2023-02-26 14:55:41 +0100

    clean code and check env vars

--- logic-pool-mqtt/main.py
+++ logic-pool-mqtt/main.py
@@ -12,20 +12,20 @@ import lights
 import power
 import sensors
 
-if os.environ.get("MQTT_PASS", "pass") == "pass":
+# Load env vars
+if os.environ.get("MQTT_PASS", "no_set") == "no_set":
   from dotenv import load_dotenv
   load_dotenv(dotenv_path="../.env")
 
-MQTT_USER = os.environ.get("MQTT_USER", "user")
-MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
-MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
+MQTT_USER = os.environ.get("MQTT_USER", "no_set")
+MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
+MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
+HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "no_set")
+HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no_set")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "no_set")
+
+# Define constants
 MQTT_PORT = 1883
-
-HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "localhost")
-HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no-token")
-
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "no-token")
-
 TOPICS = [
   "heartbeats/request",
   "device/rgb001/color",
@@ -45,16 +45,24 @@ TOPICS = [
   "device/switch_radiator/on",
 ]
 
+# Instantiate objects
 mqtt_client = mqtt.Client(client_id="logic-pool-mqtt")
-openai.api_key = OPENAI_API_KEY
 homeware = Homeware(mqtt_client, HOMEWARE_API_URL, HOMEWARE_API_KEY)
 alert = Alert(mqtt_client, openai)
 
+# Suscribe to topics on connect
+def on_connect(client, userdata, flags, rc):
+  for topic in TOPICS:
+    client.subscribe(topic)
+
+# Do tasks when a message is received
 def on_message(client, userdata, msg):
   if True:
     if msg.topic == "heartbeats/request":
+      # Send heartbeat
       mqtt_client.publish("heartbeats", "logic-pool-mqtt")
     else:
+      # Exec the logic
       payload = functions.loadPayload(msg.payload)
       if payload is not None:
         switches.green(homeware, msg.topic, payload)
@@ -69,24 +77,37 @@ def on_message(client, userdata, msg):
         sensors.hood(homeware, msg.topic, payload)
   # except Exception as e:
   #   mqtt_client.publish("message-alerts", "Excepción en Logic pool mqtt")
-  #   mqtt_client.publish("message-alerts", str(e))
-
-
-def on_connect(client, userdata, flags, rc):
-  for topic in TOPICS:
-    client.subscribe(topic)
-
-def main():
-  # Define callbacks
-  mqtt_client.on_message = on_message
-  mqtt_client.on_connect = on_connect
-  # Create connection with the MQTT broker
-  mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
-  mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
-  # Send boot message
-  mqtt_client.publish("message-alerts", "Logic pool mqtt: operativo")
-  # Begin the loop
-  mqtt_client.loop_forever()
+  #   mqtt_client.publish("message-alerts", str(e)) 
 
 if __name__ == "__main__":
-  main()
+  # Check env vars
+  if MQTT_USER == "no_set":
+    print("MQTT_USER env vars no set")
+    exit()
+  if MQTT_PASS == "no_set":
+    print("MQTT_PASS env vars no set")
+    exit()
+  if MQTT_HOST == "no_set":
+    print("MQTT_HOST env vars no set")
+    exit()
+  if HOMEWARE_API_URL == "no_set":
+    print("HOMEWARE_API_URL env vars no set")
+    exit()
+  if HOMEWARE_API_KEY == "no_set":
+    print("HOMEWARE_API_KEY env vars no set")
+    exit()
+  if OPENAI_API_KEY == "no_set":
+    print("OPENAI_API_KEY env vars no set")
+    exit()
+  # Set the API key for OpenAI
+  openai.api_key = OPENAI_API_KEY
+  # Declare the callback functions
+  mqtt_client.on_message = on_message
+  mqtt_client.on_connect = on_connect
+  # Connect to the mqtt broker
+  mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
+  mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
+  # Wake up alert
+  mqtt_client.publish("message-alerts", "Logic pool mqtt: operativo")
+  # Main loop
+  mqtt_client.loop_forever()
