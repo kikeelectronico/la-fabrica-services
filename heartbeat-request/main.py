486171a2ed67c94fd1f6b173a2c4f5f967215f42
enrique.gomez <enrique.gomez@bq.com>
2023-02-26 13:21:26 +0100

    clean heartbeat request

--- heartbeat-request/main.py
+++ heartbeat-request/main.py
@@ -2,24 +2,44 @@ import paho.mqtt.client as mqtt
 import os
 import time
 
-
-if os.environ.get("MQTT_PASS", "pass") == "pass":
+# Load env vars
+if os.environ.get("MQTT_PASS", "no_set") == "no_set":
   from dotenv import load_dotenv
   load_dotenv(dotenv_path="../.env")
 
-MQTT_USER = os.environ.get("MQTT_USER", "user")
-MQTT_PASS = os.environ.get("MQTT_PASS", "pass")
-MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
-MQTT_PORT = 1883
+MQTT_USER = os.environ.get("MQTT_USER", "no_set")
+MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
+MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
 
+# Define constants
+MQTT_PORT = 1883
+SLEEP_TIME = 10
+
+# Instantiate objects
 mqtt_client = mqtt.Client(client_id="heartbeat-request")
 
+# Main entry point
 if __name__ == "__main__":
+  # Check env vars
+  if MQTT_HOST == "no_set":
+    print("MQTT_HOST env vars no set")
+    exit()
+  if MQTT_PASS == "no_set":
+    print("MQTT_PASS env vars no set")
+    exit()
+  if MQTT_HOST == "no_set":
+    print("MQTT_HOST env vars no set")
+    exit()
+  
+  # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
   mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
+  # Wake up alert
   mqtt_client.publish("message-alerts", "Heartbeat request: operativo")
+  # Main loop
   while True:
+    # Send the heartbeat request and wait
     mqtt_client.publish("heartbeats/request", "are-you-there")
-    time.sleep(10)
+    time.sleep(SLEEP_TIME)
 
       
