d5a20ea20ba83a972e56921616f51ac0c18ffecc
enrique.gomez <enrique.gomez@bq.com>
2023-03-09 14:44:12 +0100

    request every 5 seconds

--- ble-sensors-2-mqtt/main.py
+++ ble-sensors-2-mqtt/main.py
@@ -1,7 +1,9 @@
 from bluepy import btle
 
 # MAC address
-BLE_ADDRESS = "F1:1C:1C:76:7F:3C"
+DEVICES_MACS = {
+  "livingroom": "F1:1C:1C:76:7F:3C"
+}
 # API UUIDs
 API_SERVICE_UUID ="cba20d00-224d-11e6-9fb8-0002a5d5c51b"
 API_TX_CHARACTERISTIC_UUID= "cba20003-224d-11e6-9fb8-0002a5d5c51b"
@@ -20,14 +22,12 @@ class MyDelegate(btle.DefaultDelegate):
             temp = temp_int + (temp_dec/10)
             hum = data[3] if data[3] < 128 else (data[3] - 128)
             print(temp, hum)
-            exit()
         elif data[0] == 7:
             print("low batery")
-            exit()
     
 
 print("Connecting...")
-dev = btle.Peripheral(BLE_ADDRESS, btle.ADDR_TYPE_RANDOM)
+dev = btle.Peripheral(DEVICES_MACS["livingroom"], btle.ADDR_TYPE_RANDOM)
 dev.withDelegate( MyDelegate() )
 # Get the API service
 service_uuid = btle.UUID(API_SERVICE_UUID)
@@ -37,12 +37,12 @@ tx_uuid = btle.UUID(API_TX_CHARACTERISTIC_UUID)
 tx_char = ble_service.getCharacteristics(tx_uuid)[0]
 setup_data = b"\x01\x00"
 dev.writeCharacteristic(tx_char.valHandle + 1, setup_data)
-# Request temperature and humidity
-rx_uuid = btle.UUID(API_RX_CHARACTERISTIC_UUID)
-rx_char = ble_service.getCharacteristics(rx_uuid)[0]
-rx_char.write(bytes.fromhex("570f31"))
 
 while True:
-    if dev.waitForNotifications(1.0):
+    if dev.waitForNotifications(5):
         continue
     print("Waiting...")
+    # Request temperature and humidity
+    rx_uuid = btle.UUID(API_RX_CHARACTERISTIC_UUID)
+    rx_char = ble_service.getCharacteristics(rx_uuid)[0]
+    rx_char.write(bytes.fromhex("570f31"))
