fe4461f7ee894b4dd81abcc68ed04c4b803c953b
enrique.gomez <enrique.gomez@bq.com>
2023-03-09 19:02:54 +0100

    add MQTT_HOST_NETWORK

--- ble-sensors-2-mqtt/main.py
+++ ble-sensors-2-mqtt/main.py
@@ -11,7 +11,7 @@ if os.environ.get("MQTT_PASS", "no_set") == "no_set":
 
 MQTT_USER = os.environ.get("MQTT_USER", "no_set")
 MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
-MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
+MQTT_HOST = os.environ.get("MQTT_HOST_NETWORK", "no_set")
 
 # Define constants
 MQTT_PORT = 1883
