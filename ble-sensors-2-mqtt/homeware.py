d00825a4fcf47a2301a3065c6c3ce4ffa45bf330
enrique.gomez <enrique.gomez@bq.com>
2023-03-09 15:02:06 +0100

    add a homeware connector

---
+++ ble-sensors-2-mqtt/homeware.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+class Homeware:
+
+  __mqtt_client = None
+  __url = "localhost"
+  __token = "token"
+
+  def __init__(self, mqtt_client):
+    self.__mqtt_client = mqtt_client
+
+  # Make an execution request to Homeware API
+  def execute(self, id, param, value):    
+    control_payload = {
+      "id": id,
+      "param": param,
+      "value": value,
+      "intent": "execute"
+    }
+    self.__mqtt_client.publish("device/control", json.dumps(control_payload))
--- ble-sensors-2-mqtt/main.py
+++ ble-sensors-2-mqtt/main.py
@@ -1,14 +1,34 @@
 from bluepy import btle
+import os
+import paho.mqtt.client as mqtt
 
+from homeware import Homeware
+
+if os.environ.get("MQTT_PASS", "no_set") == "no_set":
+  from dotenv import load_dotenv
+  load_dotenv(dotenv_path="../.env")
+
+MQTT_USER = os.environ.get("MQTT_USER", "no_set")
+MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
+MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
+
+# Define constants
+MQTT_PORT = 1883
 # MAC address
-DEVICES_MACS = {
-  "livingroom": "F1:1C:1C:76:7F:3C"
+DEVICES = {
+  "livingroom": {
+    "mac": "F1:1C:1C:76:7F:3C",
+    "homeware_id": "thermostat_livingroom"
+  }
 }
 # API UUIDs
 API_SERVICE_UUID ="cba20d00-224d-11e6-9fb8-0002a5d5c51b"
 API_TX_CHARACTERISTIC_UUID= "cba20003-224d-11e6-9fb8-0002a5d5c51b"
 API_RX_CHARACTERISTIC_UUID= "cba20002-224d-11e6-9fb8-0002a5d5c51b"
 
+# Instantiate objects
+mqtt_client = mqtt.Client(client_id="ble-sensors-2-mqtt")
+homeware = Homeware(mqtt_client, )
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
@@ -22,23 +42,42 @@ class MyDelegate(btle.DefaultDelegate):
             temp = temp_int + (temp_dec/10)
             hum = data[3] if data[3] < 128 else (data[3] - 128)
             print(temp, hum)
+            homeware.execute(DEVICES["livingroom"]["homeware_id"],"thermostatTemperatureAmbient",temp)
         elif data[0] == 7:
             print("low batery")
-    
 
-print("Connecting...")
-dev = btle.Peripheral(DEVICES_MACS["livingroom"], btle.ADDR_TYPE_RANDOM)
-dev.withDelegate( MyDelegate() )
-# Get the API service
-service_uuid = btle.UUID(API_SERVICE_UUID)
-ble_service = dev.getServiceByUUID(service_uuid)
-# Set the notifications
-tx_uuid = btle.UUID(API_TX_CHARACTERISTIC_UUID)
-tx_char = ble_service.getCharacteristics(tx_uuid)[0]
-setup_data = b"\x01\x00"
-dev.writeCharacteristic(tx_char.valHandle + 1, setup_data)
+# Main entry point
+if __name__ == "__main__":
+  # Check env vars
+  if MQTT_USER == "no_set":
+    print("MQTT_USER env vars no set")
+    exit()
+  if MQTT_PASS == "no_set":
+    print("MQTT_PASS env vars no set")
+    exit()
+  if MQTT_HOST == "no_set":
+    print("MQTT_HOST env vars no set")
+    exit()
+  
+  # Connect to the mqtt broker
+  mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
+  mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
+  # Wake up alert
+  #mqtt_client.publish("message-alerts", "Hue 2 MQTT: operativo")
 
-while True:
+  print("Connecting...")
+  dev = btle.Peripheral(DEVICES["livingroom"]["mac"], btle.ADDR_TYPE_RANDOM)
+  dev.withDelegate( MyDelegate() )
+  # Get the API service
+  service_uuid = btle.UUID(API_SERVICE_UUID)
+  ble_service = dev.getServiceByUUID(service_uuid)
+  # Set the notifications
+  tx_uuid = btle.UUID(API_TX_CHARACTERISTIC_UUID)
+  tx_char = ble_service.getCharacteristics(tx_uuid)[0]
+  setup_data = b"\x01\x00"
+  dev.writeCharacteristic(tx_char.valHandle + 1, setup_data)
+
+  while True:
     if dev.waitForNotifications(5):
         continue
     print("Waiting...")
