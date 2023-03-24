05e186b4ee3f53387b795480b446b384e5f13d58
enrique.gomez <enrique.gomez@bq.com>
2023-03-25 00:38:30 +0100

    add logger to hue 2 mqtt

--- hue-2-mqtt/hue.py
+++ hue-2-mqtt/hue.py
@@ -5,12 +5,22 @@ class Hue:
   __url = "localhost"
   __token = "token"
 
-  def __init__(self, url, token):
+  def __init__(self, url, token, logger):
     self.__url = url
     self.__token = token
+    self.logger = logger
     
   # Get lights json
   def getLights(self):
-    url = "http://" + self.__url + "/api/" +	self.__token + "/lights"
-    response = requests.get(url)
-    return response.json()
+    try:
+      url = "http://" + self.__url + "/api/" +	self.__token + "/lights"
+      response = requests.get(url)
+      if response.status_code == 200:
+        return response.json()
+      else:
+        self.logger.log_text("Fail to get Hue Bridge lights. Status code: " + str(response.status_code), severity="WARNING")
+        return {}
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        self.logger.log_text("Fail to get Hue Bridge lights. Conection error.", severity="WARNING")
+        self._fail_to_update = False
+        return {}
--- hue-2-mqtt/main.py
+++ hue-2-mqtt/main.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import google.cloud.logging as logging
 import os
 import time
 
@@ -17,49 +18,48 @@ HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "no_set")
 HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no_set")
 HUE_HOST = os.environ.get("HUE_HOST", "no_set")
 HUE_TOKEN = os.environ.get("HUE_TOKEN", "no_set")
+ENV = os.environ.get("ENV", "dev")
 
 # Define constants
 MQTT_PORT = 1883
 SLEEP_TIME = 10
+SERVICE = "hue-2-mqtt-" + ENV
 
 # Declare variables
 cache = {}
 
 # Instantiate objects
-mqtt_client = mqtt.Client(client_id="hue-2-mqtt")
+mqtt_client = mqtt.Client(client_id=SERVICE)
+logger = logging.Client().logger(SERVICE)
 homeware = Homeware(mqtt_client, HOMEWARE_API_URL, HOMEWARE_API_KEY)
-hue = Hue(HUE_HOST, HUE_TOKEN)
+hue = Hue(HUE_HOST, HUE_TOKEN, logger)
 
 # Main entry point
 if __name__ == "__main__":
+  logger.log_text("Starting", severity="INFO")
   # Check env vars
+  def report(message):
+    print(message)
+    logger.log_text(message, severity="ERROR")
+    exit()
   if MQTT_USER == "no_set":
-    print("MQTT_USER env vars no set")
-    exit()
+    report("MQTT_USER env vars no set")
   if MQTT_PASS == "no_set":
-    print("MQTT_PASS env vars no set")
-    exit()
+    report("MQTT_PASS env vars no set")
   if MQTT_HOST == "no_set":
-    print("MQTT_HOST env vars no set")
-    exit()
+    report("MQTT_HOST env vars no set")
   if HOMEWARE_API_URL == "no_set":
-    print("HOMEWARE_API_URL env vars no set")
-    exit()
+    report("HOMEWARE_API_URL env vars no set")
   if HOMEWARE_API_KEY == "no_set":
-    print("HOMEWARE_API_KEY env vars no set")
-    exit()
+    report("HOMEWARE_API_KEY env vars no set")
   if HUE_HOST == "no_set":
-    print("HUE_HOST env vars no set")
-    exit()
+    report("HUE_HOST env vars no set")
   if HUE_TOKEN == "no_set":
-    print("HUE_TOKEN env vars no set")
-    exit()
+    report("HUE_TOKEN env vars no set")
   
   # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
   mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
-  # Wake up alert
-  mqtt_client.publish("message-alerts", "Hue 2 MQTT: operativo")
   # Main loop
   while True:
     # Check online lights
@@ -73,6 +73,7 @@ if __name__ == "__main__":
               homeware.execute("hue_" + device_id,
                               "online",
                               device["state"]["reachable"])
+              logger.log_text(device_id + ": " + "arriba" if device["state"]["reachable"] else "caido", severity="WARNING")
           else:
             cache["hue_" + device_id] = device["state"]
             homeware.execute("hue_" + device_id,
