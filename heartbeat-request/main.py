3fe60bc2334e2be097f0ac9231763b4fa91e0fdc
enrique.gomez <enrique.gomez@bq.com>
2023-03-25 00:24:01 +0100

    add logger to heartbeat-request

--- heartbeat-request/main.py
+++ heartbeat-request/main.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import google.cloud.logging as logging
 import os
 import time
 
@@ -10,33 +11,36 @@ if os.environ.get("MQTT_PASS", "no_set") == "no_set":
 MQTT_USER = os.environ.get("MQTT_USER", "no_set")
 MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
 MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
+ENV = os.environ.get("ENV", "dev")
 
 # Define constants
 MQTT_PORT = 1883
 SLEEP_TIME = 10
+SERVICE = "alert-system-requests-" + ENV
 
 # Instantiate objects
-mqtt_client = mqtt.Client(client_id="heartbeat-request")
+mqtt_client = mqtt.Client(client_id=SERVICE)
+logger = logging.Client().logger(SERVICE)
 
 # Main entry point
 if __name__ == "__main__":
+  logger.log_text("Starting", severity="INFO")
   # Check env vars
+  def report(message):
+    print(message)
+    logger.log_text(message, severity="ERROR")
+    exit()
   if MQTT_USER == "no_set":
-    print("MQTT_USER env vars no set")
-    exit()
+    report("MQTT_USER env vars no set")
   if MQTT_PASS == "no_set":
-    print("MQTT_PASS env vars no set")
-    exit()
+    report("MQTT_PASS env vars no set")
   if MQTT_HOST == "no_set":
-    print("MQTT_HOST env vars no set")
-    exit()
+    report("MQTT_HOST env vars no set")
   
   # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
   mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
   # Wake up alert
-  mqtt_client.publish("message-alerts", "Heartbeat request: operativo")
-  # Main loop
   while True:
     # Send the heartbeat request and wait
     mqtt_client.publish("heartbeats/request", "are-you-there")
