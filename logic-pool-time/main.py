9a4cdc1e0a088365ca4a0613cdaf85c6935da7e0
enrique.gomez <enrique.gomez@bq.com>
2023-03-25 14:34:10 +0100

    add logger to logic pool time

--- logic-pool-time/main.py
+++ logic-pool-time/main.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import google.cloud.logging as logging
 import datetime
 import os
 import time
@@ -15,46 +16,48 @@ MQTT_PASS = os.environ.get("MQTT_PASS", "no_set")
 MQTT_HOST = os.environ.get("MQTT_HOST", "no_set")
 HOMEWARE_API_URL = os.environ.get("HOMEWARE_API_URL", "no_set")
 HOMEWARE_API_KEY = os.environ.get("HOMEWARE_API_KEY", "no_set")
+ENV = os.environ.get("ENV", "dev")
+
 
 # Define constants
 MQTT_PORT = 1883
+SERVICE = "logic-pool-time-" + ENV
 
 # Declare variables
 last_heartbeat_timestamp = 0
 just_executed = ""
 
 # Instantiate objects
-mqtt_client = mqtt.Client(client_id="logic-pool-time")
-homeware = Homeware(mqtt_client, HOMEWARE_API_URL, HOMEWARE_API_KEY)
+mqtt_client = mqtt.Client(client_id=SERVICE)
+logger = logging.Client().logger(SERVICE)
+homeware = Homeware(mqtt_client, HOMEWARE_API_URL, HOMEWARE_API_KEY, SERVICE)
 
 def main():
   global last_heartbeat_timestamp
   global just_executed
+  today = datetime.datetime.now()
+  hour = today.strftime("%H:%M:%S")
+  logger.log_text("Starting", severity="INFO")
+  logger.log_text("Hora local " + str(hour), severity="INFO")
   # Check env vars
+  def report(message):
+    print(message)
+    logger.log_text(message, severity="ERROR")
+    exit()
   if MQTT_USER == "no_set":
-    print("MQTT_USER env vars no set")
-    exit()
+    report("MQTT_USER env vars no set")
   if MQTT_PASS == "no_set":
-    print("MQTT_PASS env vars no set")
-    exit()
+    report("MQTT_PASS env vars no set")
   if MQTT_HOST == "no_set":
-    print("MQTT_HOST env vars no set")
-    exit()
+    report("MQTT_HOST env vars no set")
   if HOMEWARE_API_URL == "no_set":
-    print("HOMEWARE_API_URL env vars no set")
-    exit()
+    report("HOMEWARE_API_URL env vars no set")
   if HOMEWARE_API_KEY == "no_set":
-    print("HOMEWARE_API_KEY env vars no set")
-    exit()
+    report("HOMEWARE_API_KEY env vars no set")
 
   # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
   mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
-  # Wake up alert
-  today = datetime.datetime.now()
-  hour = today.strftime("%H:%M:%S")
-  mqtt_client.publish("message-alerts", "Logic pool time: operativo")
-  mqtt_client.publish("message-alerts", "Hora local " + str(hour))
   # Main loop
   while True:
     # Get current time
