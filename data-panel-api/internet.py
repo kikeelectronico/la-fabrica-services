0c05b7ca847ba626853f9dfc35507de2caadf780
enrique.gomez <enrique.gomez@bq.com>
2023-05-12 21:17:43 +0200

    check both services for verify internet connection

--- data-panel-api/internet.py
+++ data-panel-api/internet.py
@@ -9,14 +9,20 @@ class Internet:
 
   def checkConnectivity(self):
     try:
-      requests.get("https://www.google.com", timeout=5)
-      requests.get("https://www.cloudflare.com/", timeout=5)
+      requests.get("https://www.google.com", timeout=2)
       self._connected = True
-      return True
     except (requests.ConnectionError, requests.Timeout) as exception:
-      self.logger.log_text("Fail to reach Google or Cloudflare. Conection error.", severity="WARNING")
+      self.logger.log_text("Fail to reach Google. Conection error.", severity="WARNING")
       self._connected = False
-      return False
+      
+    try:
+      requests.get("https://www.cloudflare.com/", timeout=2)
+      self._connected = True
+    except (requests.ConnectionError, requests.Timeout) as exception:
+      self.logger.log_text("Fail to reach Cloudflare. Conection error.", severity="WARNING")
+      self._connected = self._connected and False
+    
+    return self._connected
 
   def getConnected(self):
     return self._connected
