1d74727ab75442cce452a886d36194be7dd6deca
enrique.gomez <enrique.gomez@bq.com>
2023-05-14 21:14:33 +0200

    change hue_sensor_2 status instead of hue_2 and hue_3

--- logic-pool-mqtt/switches.py
+++ logic-pool-mqtt/switches.py
@@ -50,16 +50,13 @@ def bathroom(homeware, topic, payload):
   if topic == "device/hue_sensor_14/on":
     if payload:
       if homeware.get("scene_dim","enable"):
-        homeware.execute("hue_2","on",True)
-        homeware.execute("hue_3","on",True)
+        homeware.execute("hue_sensor_2","on",True)
         homeware.execute("light001","on",False)
       else:
-        homeware.execute("hue_2","on",False)
-        homeware.execute("hue_3","on",False)
+        homeware.execute("hue_sensor_2","on",False)
         homeware.execute("light001","on",True)
     else:
-      homeware.execute("hue_2","on",False)
-      homeware.execute("hue_3","on",False)
+      homeware.execute("hue_sensor_2","on",False)
       homeware.execute("light001","on",False)
 
 def mirror(homeware, topic, payload):
