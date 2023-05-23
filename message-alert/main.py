5bf7d39059ff10b8f75cbeaa7bdcfbce9fede8ca
enrique.gomez <enrique.gomez@bq.com>
2023-05-23 21:16:09 +0200

    change logger

--- message-alert/main.py
+++ message-alert/main.py
@@ -1,7 +1,8 @@
 import paho.mqtt.client as mqtt
 import telebot
 import os
-import google.cloud.logging as logging
+
+from logger import Logger
 
 # Load env vars
 if os.environ.get("MQTT_PASS", "no_set") == "no_set":
@@ -22,7 +23,7 @@ SERVICE = "message-alert-" + ENV
 
 # Instantiate objects
 mqtt_client = mqtt.Client(client_id=SERVICE)
-logger = logging.Client().logger(SERVICE)
+logger = Logger(mqtt_client, SERVICE)
 bot = telebot.TeleBot(token=BOT_TOKEN)
 
 # Suscribe to topics on connect
@@ -42,11 +43,10 @@ def on_message(client, userdata, msg):
 	
 # Main entry point
 if __name__ == "__main__":
-  logger.log_text("Starting", severity="INFO")
   # Check env vars
   def report(message):
     print(message)
-    logger.log_text(message, severity="ERROR")
+    #logger.log(message, severity="ERROR")
     exit()
   if MQTT_USER == "no_set":
     report("MQTT_USER env vars no set")
@@ -65,6 +65,7 @@ if __name__ == "__main__":
   # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
   mqtt_client.connect(MQTT_HOST, MQTT_PORT, 60)
+  logger.log("Starting", severity="INFO")
   # Main loop
   mqtt_client.loop_forever()
 
