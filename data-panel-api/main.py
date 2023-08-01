30a4fe4a25a587c632457a3c44c83ee7a81999dd
Enrique <enrique.gomez.electronico@gmail.com>
2023-08-01 13:58:25 +0200

    add uvicorn to main

--- data-panel-api/main.py
+++ data-panel-api/main.py
@@ -131,3 +131,12 @@ async def alertsEndPoint():
       })
 
   return alerts
+
+if __name__ == "__main__":
+   import uvicorn
+   uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
