d9521de0d886e7353a33fa8b7acccff3962cca80
Enrique <enrique.gomez.electronico@gmail.com>
2023-11-04 14:02:37 +0100

    add support for get resource at clip v2 api

--- hue-2-mqtt/hue.py
+++ hue-2-mqtt/hue.py
@@ -9,41 +9,25 @@ class Hue:
     self.__url = url
     self.__token = token
     self.logger = logger
-    
-  # Get lights json
-  def getLights(self):
+      
+  # Get motion
+  def getResource(self, resource="device"):
     if self.__token == "no_set" or self.__url == "no_set":
       self._fail_to_update = True
       self.logger.log("Hue env vars aren't set", severity="ERROR")
     else:
       try:
-        url = "http://" + self.__url + "/api/" +	self.__token + "/lights"
-        response = requests.get(url)
+        url = "https://" + self.__url + "/clip/v2/resource/" + resource
+        headers = {
+          'hue-application-key': self.__token
+        }
+        response = requests.get(url, headers=headers, verify=False)
         if response.status_code == 200:
-          return response.json()
+          return response.json()["data"]
         else:
-          self.logger.log("Fail to get Hue Bridge lights. Status code: " + str(response.status_code), severity="WARNING")
+          self.logger.log("Fail to get Hue Bridge " + resource + ". Status code: " + str(response.status_code), severity="WARNING")
           return {}
       except (requests.ConnectionError, requests.Timeout) as exception:
-          self.logger.log("Fail to get Hue Bridge lights. Conection error.", severity="WARNING")
+          self.logger.log("Fail to get Hue Bridge " + resource + ". Conection error.", severity="WARNING")
           self._fail_to_update = False
           return {}
-      
-  # Get sesnors json
-  def getSensors(self):
-    if self.__token == "no_set" or self.__url == "no_set":
-      self._fail_to_update = True
-      self.logger.log("Hue env vars aren't set", severity="ERROR")
-    else:
-      try:
-        url = "http://" + self.__url + "/api/" +	self.__token + "/sensors"
-        response = requests.get(url)
-        if response.status_code == 200:
-          return response.json()
-        else:
-          self.logger.log("Fail to get Hue Bridge sensors. Status code: " + str(response.status_code), severity="WARNING")
-          return {}
-      except (requests.ConnectionError, requests.Timeout) as exception:
-        self.logger.log("Fail to get Hue Bridge sensors. Conection error.", severity="WARNING")
-        self._fail_to_update = False
-        return {}
