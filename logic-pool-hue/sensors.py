159b022ef6ab68ab267755c7b6fdf05454bfc2bf
Enrique <enrique.gomez.electronico@gmail.com>
2023-11-06 20:43:16 +0100

    add hall and bathroom sensors

--- logic-pool-hue/sensors.py
+++ logic-pool-hue/sensors.py
@@ -15,6 +15,29 @@ def bedroom(service, homeware):
           homeware.execute("hue_6","on",False)
           homeware.execute("rgb003","on",False)
 
+def bathroom(service, homeware):
+  if service["id"] == "73ef0d76-de9f-4cd1-b460-ec626fbc70fc":
+    state = service["motion"]["motion"]
+    if state:
+      if homeware.get("scene_dim","enable"):
+        homeware.execute("hue_sensor_2","on",True)
+        homeware.execute("light001","on",False)
+      else:
+        homeware.execute("light001","on",True)
+        homeware.execute("hue_sensor_2","on",False)
+    else:
+        if not homeware.get("hue_sensor_14", "on"):
+          homeware.execute("hue_sensor_2","on",False)
+          homeware.execute("light001","on",False)
+
+def hall(service, homeware):
+  if service["id"] == "918cdad4-9c5e-40f7-9ef2-e6a64072a2ae":
+    state = service["motion"]["motion"]
+    if state:
+      homeware.execute("hue_7","on",False)
+    else:
+      homeware.execute("hue_7","on",False)
+
 
 
 
