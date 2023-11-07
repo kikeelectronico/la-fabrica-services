e8deb542ad9ada17e0f3f31e10ed123fbd8fac70
Enrique <enrique.gomez.electronico@gmail.com>
2023-11-07 22:36:24 +0100

    disable motion sensor on scene activated

--- logic-pool-hue/sensors.py
+++ logic-pool-hue/sensors.py
@@ -4,12 +4,13 @@ def bedroom(service, homeware):
   if service["id"] == "f3a99b17-a6cb-4b51-9da6-c9a90b1eda65":
     state = service["motion"]["motion"]
     if state:
-      if homeware.get("scene_dim","enable"):
-        homeware.execute("rgb003","on",True)
-        homeware.execute("hue_6","on",False)
-      else:
-        homeware.execute("hue_6","on",True)
-        homeware.execute("rgb003","on",False)
+      if not homeware.get("scene_sensor_off","enable"):
+        if homeware.get("scene_dim","enable"):
+          homeware.execute("rgb003","on",True)
+          homeware.execute("hue_6","on",False)
+        else:
+          homeware.execute("hue_6","on",True)
+          homeware.execute("rgb003","on",False)
     else:
         if not homeware.get("hue_sensor_12", "on"):
           homeware.execute("hue_6","on",False)
