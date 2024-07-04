3d009a2560835898c07c3bc81e62fb09dddf62c1
Enrique <enrique.gomez.electronico@gmail.com>
2024-07-04 13:29:45 +0200

    clean code

--- monitor-http-request/main.py
+++ monitor-http-request/main.py
@@ -33,22 +33,14 @@ if __name__ == "__main__":
   # Check env vars
   def report(message):
     print(message)
-    #logger.log(message, severity="ERROR")
     exit()
-  if MQTT_USER == "no_set":
-    report("MQTT_USER env vars no set")
-  if MQTT_PASS == "no_set":
-    report("MQTT_PASS env vars no set")
-  if MQTT_HOST == "no_set":
-    report("MQTT_HOST env vars no set")
-  if HOMEWARE_API_URL == "no_set":
-    report("HOMEWARE_API_URL env vars no set")
-  if HOMEWARE_API_KEY == "no_set":
-    report("HOMEWARE_API_KEY env vars no set")
-  if HUE_HOST == "no_set":
-    report("HUE_HOST env vars no set")
-  if HUE_TOKEN == "no_set":
-    report("HUE_TOKEN env vars no set")
+  if MQTT_USER == "no_set": report("MQTT_USER env vars no set")
+  if MQTT_PASS == "no_set": report("MQTT_PASS env vars no set")
+  if MQTT_HOST == "no_set": report("MQTT_HOST env vars no set")
+  if HOMEWARE_API_URL == "no_set": report("HOMEWARE_API_URL env vars no set")
+  if HOMEWARE_API_KEY == "no_set": report("HOMEWARE_API_KEY env vars no set")
+  if HUE_HOST == "no_set": report("HUE_HOST env vars no set")
+  if HUE_TOKEN == "no_set": report("HUE_TOKEN env vars no set")
 
   # Connect to the mqtt broker
   mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
