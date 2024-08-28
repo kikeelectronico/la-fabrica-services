fd84a43fbb246806eff55290bd165528e6786b49
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-28 11:08:49 +0200

    update weather alert representation

--- data-panel-front/src/pages/Home.js
+++ data-panel-front/src/pages/Home.js
@@ -172,9 +172,10 @@ export default function Home(props) {
         let alert = weather.alerts.alert[i]
         let severity = "low"
         if (alert.category.includes("Extreme")) severity = "critical"
+        else if (alert.evnet.includes("Moderate")) severity = "normal"
         _weather_alerts.push(
           {
-            "text": alert.desc,
+            "text": alert.event + ": " + alert.desc,
             "severity": severity,
             "image": null
           }
