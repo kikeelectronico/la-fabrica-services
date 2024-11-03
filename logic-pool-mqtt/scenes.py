85fcec48ca04ff0b73ccacfcaf9646154de42796
Enrique <enrique.gomez.electronico@gmail.com>
2024-11-03 12:05:35 +0100

    fix comparator

--- logic-pool-mqtt/scenes.py
+++ logic-pool-mqtt/scenes.py
@@ -212,7 +212,7 @@ BATHROOM_HUMIDITY_DELTA = 20
 
 def disableShowerScene(homeware, alert, topic, payload):
   if topic == "device/c8bd20a2-69a5-4946-b6d6-3423b560ffa9/occupancy":
-    if payload > "OCCUPIED":
+    if payload == "OCCUPIED":
       if homeware.get("e5e5dd62-a2d8-40e1-b8f6-a82db6ed84f4", "openPercent") == 0:
         global initial_bathroom_humidity
         if homeware.get("thermostat_bathroom", "thermostatHumidityAmbient") > initial_bathroom_humidity + BATHROOM_HUMIDITY_DELTA:
