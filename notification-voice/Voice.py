a1237fd7733aa11a4ac64b3759ce6187b6dffc90
Enrique <enrique.gomez.electronico@gmail.com>
2025-04-06 00:47:56 +0200

    debug

--- notification-voice/Voice.py
+++ notification-voice/Voice.py
@@ -67,6 +67,7 @@ class Voice:
     self.speakers = speakers
     
   def getRoom(self):
+    print(dir(self.homeware))
     print(self.homeware.get("c8bd20a2-69a5-4946-b6d6-3423b560ffa9", "currentToggleSettings"))
     if self.homeware.get("c8bd20a2-69a5-4946-b6d6-3423b560ffa9", "currentToggleSettings")["last_seen"]:
       return "livingroom"
