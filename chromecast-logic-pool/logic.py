c0bf98e393a9938439eec9de515078e98965fa36
Enrique <enrique.gomez.electronico@gmail.com>
2025-06-03 23:53:26 +0200

    recover cooling

--- chromecast-logic-pool/logic.py
+++ chromecast-logic-pool/logic.py
@@ -18,7 +18,9 @@ def playingLights(homeware):
     time.sleep(0.5)
     homeware.execute("hue_9", "on", False)
     homeware.execute("hue_10", "on", False)
-    if homeware.get("thermostat_livingroom", "thermostatMode") == "cool":
+    prev_status.setdefault("thermostat_livingroom", {})
+    prev_status["thermostat_livingroom"]["thermostatMode"] = homeware.get("thermostat_livingroom", "thermostatMode")
+    if prev_status["thermostat_livingroom"]["thermostatMode"] == "cool":
       homeware.execute("thermostat_livingroom", "thermostatMode", "off")
     prev_player_playing_state = True
       
@@ -28,6 +30,8 @@ def notPlayingLights(homeware):
     if homeware.get("hue_4","on"):
       homeware.execute("hue_4", "brightness", prev_status["hue_4"]["brightness"])
       homeware.execute("hue_5", "brightness", prev_status["hue_5"]["brightness"])
+    if prev_status["thermostat_livingroom"]["thermostatMode"] == "cool":
+      homeware.execute("thermostat_livingroom", "thermostatMode", "cool")
     # if homeware.get("c8bd20a2-69a5-4946-b6d6-3423b560ffa9", "brightness") < 25:
     #   homeware.execute("hue_1", "on", True)
     #   homeware.execute("hue_9", "on", True)
