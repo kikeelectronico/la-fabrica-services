d3ced4833b2557c57891560c8a29b6ef311f15af
Enrique <enrique.gomez.electronico@gmail.com>
2025-07-05 00:23:01 +0200

    change asserts fot turnning the fan off

--- hue-logic-pool/sensors.py
+++ hue-logic-pool/sensors.py
@@ -75,9 +75,11 @@ def bedroom(service, homeware, mqtt_client):
                 },
                 "asserts": [
                   {
-                    "device_id": "c8bd20a2-69a5-4946-b6d6-3423b560ffa9",
-                    "param": "occupancy",
-                    "value": "OCCUPIED"
+                    "device_id": "c2b38173-883e-4766-bcb5-0cce2dc0e00e",
+                    "param": "currentToggleSettings",
+                    "value": {
+                      "last_seen": False
+                    }
                   },
                   {
                     "device_id": "scene_summer",
